f7cdd83bda2ca403add3c36f9a4066f893359a93
* <winsalva396@gmail.com>
2021-11-13 22:01:08 +0800

    fix embedded youtube url and set elixir version to 1.12.1

--- lib/my_tube_web/views/api/upload_view.ex
+++ lib/my_tube_web/views/api/upload_view.ex
@@ -2,6 +2,6 @@ defmodule MyTubeWeb.Api.UploadView do
   use MyTubeWeb, :view
 
   def render("index.json", %{uploads: uploads}) do
-    %{data: Enum.map(uploads, fn upload -> %{title: upload.title, description: upload.description, file: upload.file} end)}
+    %{data: Enum.map(uploads, fn upload -> %{title: upload.title, description: upload.description, file: url(upload.file)} end)}
   end
 end
